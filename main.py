cc50fd2a2d515dcce8dcc2ebdca2799efcc3c94d
Albertovdc <albertovdc.gnz@gmail.com>
2024-01-09 16:22:15 -0600

    *

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 question = Question(question_data[1]["text"], question_data[1]["answer"])
 
-print(question.text)
-print(question.answer)
+# print(question.text)
+# print(question.answer)
 
 
 for data in question_data:
@@ -14,4 +15,7 @@ for data in question_data:
   new_question = Question(data["text"], data["answer"])
   question_bank.append(new_question)
 
-print(question_bank)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+  quiz.next_question()
